a986aba26d1d82a38055aa70ab2465e7f1d2ecfb
adrilgrc <adrianlimagarcia00@gmail.com>
2022-01-23 19:07:17 +0000

    M

--- cpp/random/books/books.h
+++ cpp/random/books/books.h
@@ -10,14 +10,28 @@ class Book {
   int year_;
   double price_;
  public:
+  // Constructor by default
   Book() {
     name_ = "NULL";
     year_ = 0;
     price_ = 0;
   }
+  // Constructor with parameters
   Book(std::string& input_name, int& input_year, double& input_price) {
     name_ = input_name;
     year_ = input_year;
     price_ = input_price;
   }
+  // Getters
+  std::string GetName() const {
+    return name_;
+  }
+  int GetYear() const {
+    return year_;
+  }
+  double GetPrice() const {
+    return price_;
+  }
+  // Methods of the class
+  double PriceWithTaxes (int& taxes_to_apply);
 };
