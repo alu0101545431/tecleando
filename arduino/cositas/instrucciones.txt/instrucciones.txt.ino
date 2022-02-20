02dc2bc147d3d7e077f0865e396ba0dda3028307
adrilgrc <adrianlimagarcia00@gmail.com>
2022-02-20 22:53:39 +0000

    N

--- arduino/cositas/instrucciones.txt/instrucciones.txt.ino
+++ arduino/cositas/instrucciones.txt/instrucciones.txt.ino
@@ -1,15 +1,1 @@
 
-//** EXPLOSIÓN DE ENERGÍA A LAS 3 AM **//
-
-//** Definiciones **//
-int salida=2;      // definimos el pin salida como salida
-
-void setup() {
-  pinMode(salida,OUTPUT);    //declaramos el pin rojo como salida
-}
-
-void loop() {
- 
- digitalWrite(salida,HIGH);
- 
-}
