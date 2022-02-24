3c1ce942c5d0fab686112f5d71ddc084e9449998
adrilgrc <adrianlimagarcia00@gmail.com>
2022-02-24 16:26:09 +0000

    M

--- cpp/random/array_manipulation/substring_array.cc
+++ cpp/random/array_manipulation/substring_array.cc
@@ -1,11 +1,24 @@
-/* 
+/*
    Ejercicio propuesto 6 de AEyD
    Si b es una subcadena de a, devuelve un puntero a la posición
    de a en la que comienza b. Si b no es subcadena de a, devuelve NULL.
 */
 
-char *strstr(const char *a, const char *b) {
-  
+#include <iostream>
+
+int StringLength(const char *input) {
+  int index;
+  for (index = 0; input[index] != '\0'; index++);
+  return index;
+}
+
+char *LocateString(const char *main_string, const char *sub_string) {
+  for (int index = 0; main_string[index] == '\0' ; ++index) {
+    if (main_string[index] == sub_string[0] ) {
+
+    }
+    
+  }
 }
 
 int main(int argc, char const *argv[]) {
