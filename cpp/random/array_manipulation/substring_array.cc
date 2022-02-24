82779f3e3c464dad9fa4b072fb9bb4021e57162a
adrilgrc <adrianlimagarcia00@gmail.com>
2022-02-24 16:19:51 +0000

    M

--- cpp/random/array_manipulation/substring_array.cc
+++ cpp/random/array_manipulation/substring_array.cc
@@ -1,4 +1,12 @@
-char *strstr()
+/* 
+   Ejercicio propuesto 6 de AEyD
+   Si b es una subcadena de a, devuelve un puntero a la posición
+   de a en la que comienza b. Si b no es subcadena de a, devuelve NULL.
+*/
+
+char *strstr(const char *a, const char *b) {
+  
+}
 
 int main(int argc, char const *argv[]) {
   /* code */
