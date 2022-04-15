f7b92203ab198a876711ebbf1d14c7d448ebd4af
adrilgrc <adrianlimagarcia00@gmail.com>
2022-04-15 11:58:20 +0100

    [U]: Fixed code

--- cpp/opti/01-grafos/main.cpp
+++ cpp/opti/01-grafos/main.cpp
@@ -19,14 +19,14 @@ void menu (unsigned dirigido, char &opcion)
             {
             cout << "i. Mostrar [i]nformacion basica del grafo" << endl;
             cout << "a. Mostrar la lista de [a]dyacencia del grafo" << endl;
-	    //Aquí se añaden más opciones al menú del grafo no dirigido
+	    //Aquï¿½ se aï¿½aden mï¿½s opciones al menï¿½ del grafo no dirigido
             }
     else
             {
             cout << "i. Mostrar [i]nformacion basica del grafo" << endl;
             cout << "s. Mostrar la lista de [s]ucesores del grafo" << endl;
             cout << "p. Mostrar la lista de [p]redecesores del grafo" << endl;
-	    //Aquí se añaden más opciones al menú del grafo dirigido
+	    //Aquï¿½ se aï¿½aden mï¿½s opciones al menï¿½ del grafo dirigido
             };
     cout << "q. Finalizar el programa" << endl;
     cout << "Introduce la letra de la accion a ejecutar  > ";
@@ -34,12 +34,12 @@ void menu (unsigned dirigido, char &opcion)
 };
 
 
-// El principal es simplemente un gestor de menu, diferenciando acciones para dirigido y para no dirigido, y un menú inicial si no hay un grafo cargado
+// El principal es simplemente un gestor de menu, diferenciando acciones para dirigido y para no dirigido, y un menï¿½ inicial si no hay un grafo cargado
 int main(int argc, char *argv[])
 {
     int error_apertura;
     char nombrefichero[85], opcion;
-    clrscr();
+    // clrscr();
     //Si tenemos el nombre del primer fichero por argumento, es una excepcion, y lo intentamos cargar, si no, lo pedimos desde teclado
     if (argc > 1)
     {
@@ -55,21 +55,21 @@ int main(int argc, char *argv[])
     if (error_apertura == 1)
     {
         cout << "Error en la apertura del fichero desde argumento: revisa nombre y formato" << endl;
-        pressanykey();
-        clrscr();
+        // pressanykey();
+        //clrscr();
     }
     else
     {
         cout<< "Grafo cargado desde el fichero " << nombrefichero << endl;
-        pressanykey();
-        clrscr();
+        // pressanykey();
+        // clrscr();
         do
         {
             menu(G.Es_dirigido(), opcion);
             switch (opcion)
             {
                 case 'c' :
-                    clrscr();
+                    // clrscr();
          	    cout << "Introduce el nombre completo del fichero de datos" << endl;
                     cin >> nombrefichero;
                     G.actualizar(nombrefichero, error_apertura);
@@ -81,19 +81,19 @@ int main(int argc, char *argv[])
                     {
                         cout << "Fichero cargado correctamente desde " << nombrefichero << endl;
                     };
-                    pressanykey();
-                    clrscr();
+                    // pressanykey();
+                    // clrscr();
                     break;
 
                 case 'i' :
-                    clrscr();
+                    // clrscr();
 		    cout << "Grafo cargado desde " << nombrefichero << endl;
                     G.Info_Grafo();
-                    pressanykey();
-                    clrscr();
+                    // pressanykey();
+                    // clrscr();
                     break;
 
-		 //Situar aquí el resto de opciones del menú
+		 //Situar aquï¿½ el resto de opciones del menï¿½
             }
     }
     while (opcion != 'q');
