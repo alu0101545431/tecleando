14066b4bb6aeec01e1a005216cff9845fd053a1a
adrilgrc <adrianlimagarcia00@gmail.com>
2022-05-02 19:51:50 +0100

    [U]: Terminada, falta probar raices, etc y cambiar datos

--- cpp/aeyd/practicas/05-rpn/rpn_t.h
+++ cpp/aeyd/practicas/05-rpn/rpn_t.h
@@ -1,6 +1,6 @@
-// AUTOR: 
-// FECHA: 
-// EMAIL: 
+// AUTOR:
+// FECHA:
+// EMAIL:
 // VERSION: 2.0
 // ASIGNATURA: Algoritmos y Estructuras de Datos
 // PRÁCTICA Nº: 5
@@ -10,18 +10,19 @@
 #ifndef RPNT_H_
 #define RPNT_H_
 
-#include <iostream>
 #include <cctype>
 #include <cmath>
+#include <iostream>
 //#include <cstdlib>
 
 #include "queue_l_t.h"
 
 // Clase RPN (Reverse Polish Notation)
-template <class T> class rpn_t {
+template <class T>
+class rpn_t {
  public:
   // constructor
- rpn_t(void) : stack_() {}
+  rpn_t(void) : stack_() {}
 
   // destructor
   ~rpn_t() {}
@@ -29,52 +30,85 @@ template <class T> class rpn_t {
   // operaciones
   const int evaluate(queue_l_t<char>&);
 
- private: 
+ private:
   T stack_;
   void operate_(const char operador);
 };
 
-
 // operaciones
-template<class T> const int rpn_t<T>::evaluate(queue_l_t<char>& q) {
-  while (!q.empty()) 	{
+template <class T>
+const int rpn_t<T>::evaluate(queue_l_t<char>& q) {
+  while (!q.empty()) {
     char c = q.front();
-    
+
     q.pop();
     std::cout << "Sacamos de la cola un carácter: " << c;
 
     if (isdigit(c)) {
       int i = c - '0';
       // poner código
+      stack_.push(i);
       std::cout << " (es un dígito) " << std::endl
-		<< "   Lo metemos en la pila..." << std::endl;
+                << "   Lo metemos en la pila..." << std::endl;
     } else {
       std::cout << " (es un operador)" << std::endl;
-      // poner código
+      operate_(c);
     }
   }
+  return stack_.top();
   // poner código
 }
 
-template<class T> void rpn_t<T>::operate_(const char c) {
-  assert(c == '+' || c == '-' || c == '*' || c == '/');
-
+template <class T>
+void rpn_t<T>::operate_(const char c) {
+  assert(c == '+' || c == '-' || c == '*' || c == '/' || c == 'l' ||
+         c == '^'|| c == 'c' || c == 'r');
+  int first_operator = 0, second_operator = 0;
   // poner código
-  std::cout << "   Sacamos de la pila un operando: " << std::endl;
-  
-  // poner código
-  std::cout << "   Sacamos de la pila otro operando: " << std::endl;
-  
+  if (c == '+' || c == '-' || c == '*' || c == '/' || c == 'l' || c == '^') {
+    first_operator = stack_.top();
+    std::cout << "   Sacamos de la pila un operando: " << first_operator
+              << std::endl;
+    stack_.pop();
+    second_operator = stack_.top();
+    std::cout << "   Sacamos de la pila otro operando: " << second_operator
+              << std::endl;
+    stack_.pop();
+  } else if (c == 'c' || c == 'r') {
+    first_operator = stack_.top();
+    stack_.pop();
+  }
   switch (c) {
     case '+':
-      // poner código
+      stack_.push(first_operator + second_operator);
       break;
     // poner código resto de operadores
+    case '-':
+      stack_.push(second_operator - first_operator);  // preguntar tutorizada
+      break;
+    case '/':
+      stack_.push(first_operator / second_operator);
+      break;
+    case '*':
+      stack_.push(first_operator * second_operator);
+      break;
+    case '^':
+      stack_.push(pow(first_operator, second_operator));
+      break;
+    case 'r':
+      stack_.push(sqrt(first_operator));
+      break;
+    case 'l':
+      stack_.push(log2(first_operator));
+      break;
+    case 'c':
+      stack_.push(pow(first_operator, 2));
+      break;
   }
 
   // poner código
-  std::cout << "   Metemos en la pila el resultado: " << std::endl;
+  std::cout << "   Metemos en la pila el resultado: " << stack_.top()
+            << std::endl;
 }
 
- 
 #endif  // RPNT_H_
