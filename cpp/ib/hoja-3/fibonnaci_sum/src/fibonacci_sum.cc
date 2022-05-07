ee2167e57bf59f89c2766e6a01a4900a18d55eaf
adrilgrc <adrianlimagarcia00@gmail.com>
2022-05-07 14:37:49 +0100

    [N]: Fibonacci

---
+++ cpp/ib/hoja-3/fibonnaci_sum/src/fibonacci_sum.cc
@@ -0,0 +1,23 @@
+#include "iostream"
+#include "string"
+
+void PrintHelpMessage() {
+  std::cout
+      << "[INFO]: ./fibonacci_sum [n] where 'n' is the number to calculate"
+      << std::endl;
+}
+
+void Usage(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << argv[0] << " forgot number like argument" << std::endl
+              << "Try '" << argv[0] << " --help' for more information"
+              << std::endl;
+    std::exit(0);
+  }
+  if (argc == 2) {
+    std::string parameter = argv[1];
+    if (parameter == "--help") {
+      PrintHelpMessage();
+    }
+  }
+}
