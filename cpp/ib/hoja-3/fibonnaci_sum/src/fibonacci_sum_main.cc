be65c4d231129caa4b12d4944913559511ce48c9
adrilgrc <adrianlimagarcia00@gmail.com>
2022-05-07 15:28:18 +0100

    [U]: Fibonacci recursivo terminado

--- cpp/ib/hoja-3/fibonnaci_sum/src/fibonacci_sum_main.cc
+++ cpp/ib/hoja-3/fibonnaci_sum/src/fibonacci_sum_main.cc
@@ -4,6 +4,9 @@
 #include "fibonacci_sum.h"
 
 int main(int argc, char const *argv[]) {
+  int input_number = 0;
   Usage(argc, argv);
+  input_number = std::stoi(argv[1]);
+  std::cout << FibonacciSum(input_number) << std::endl;
   return 0;
 }
